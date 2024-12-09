907c1e3f7c8855c0195365c62bd655f7f025b09c
ikuzweshemaelia <ikuzweshemaelia@gmail.com>
2024-12-09 14:19:23 +0000

    adding form

--- lib/action.ts
+++ lib/action.ts
@@ -1,25 +1,19 @@
-"use server"
+"use server";
 
 import { parseWithZod } from "@conform-to/zod";
 import { redirect } from "next/navigation";
 import { LoginShema } from "./shema";
 
-
 export async function authenticate(prevState: unknown, formData: FormData) {
-    const validate = parseWithZod(formData, { schema: LoginShema });
-    if (validate.status !== "success") {
-        return validate.reply()
-    }
-   const{email,password}=validate.value
-   if(email=="eshemaikuzwe@gmail.com"){
-    //    return {
-    //     status:"error",
-    //     message:"Email already Exits"
-    //    }
-
-      return {
-        v
-      }
-   }
-    redirect("/")
+  const validate = parseWithZod(formData, { schema: LoginShema });
+  if (validate.status !== "success") {
+    return validate.reply({ resetForm: false });
+  }
+  const { email, password } = validate.value;
+  if (email == "eshemaikuzwe@gmail.com") {
+    return validate.reply({
+      formErrors: ["Custom error message here."],
+    }).error;
+  }
+  redirect("/");
 }
